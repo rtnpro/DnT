1298897e8275f7f903769dc906d5d2e6a7329317
Ratnadeep Debnath <ratnadeep.debnath@ibibogroup.com>
2014-05-10 16:13:31 +0530

    Added collections for projects and project milestones.

--- src/lib/models.js
+++ src/lib/models.js
@@ -1,2 +1,43 @@
-Contract = new Meteor.Collection('contract')
+/*
+Projects: {
+  name: String,
+  description: String,
+  website: String,
+  thumbnail: String(URL),
+  total_funds: float,
+  self_funds: float,
+  donations: float,
+  expenditure: float,
+  progress: float (percentage),
+  organisation_id: String (organisation id),
+  organisation_name: String,
+  status: String (open/work_in_progress),
+  creator_user_id: String,
+  creator_username: String,
+  owner_user_id: String,
+  owner_username: String,
+  created: Datetime,
+  last_updated: Datetime
+}
+*/
+Projects = new Meteor.Collection('projects');
 
+
+/*
+ProjectMilestones: {
+  name: String,
+  description: String,
+  project_id: String,
+  project_name: String,
+  progress: String,
+  total_funds: float,
+  expenditure: float,
+  progress: float,
+  status: String,
+  created: Datetime,
+  last_updated: Datetime,
+  creator_user_id: String,
+  creator_user_name: String
+}
+*/
+ProjectMilestones = new Meteor.Collection('project_milestones');
